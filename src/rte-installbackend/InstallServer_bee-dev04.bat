e69d43a0c2960a3e36439a1d896e0f9af3125c34
Hendrik du Toit <hendrik@brightedge.co.za>
2023-10-08 15:51:41 +0200

    Confugure batch files and shell scripts

--- src/rte-installbackend/InstallServer_bee-dev04.bat
+++
@@ -1,7 +0,0 @@
-@ECHO ON
-@REM "C:\Program Files\Beyond Compare 4\BCompare.exe" @"InstallServer.txt" 178.62.45.198 138.68.152.245
-ssh -t -E D:\Dropbox\Projects\RealTimeEvents\src\rte\install\Install.log root@159.65.19.237 "ufw enable;ufw allow OpenSSH;apt update;apt install openssh-server;ufw allow 22;systemctl start ssh"
-scp d:\Dropbox\Projects\RealTimeEvents\src\rte\install\InstallServer_s1.* root@159.65.19.237://usr/local/share
-ssh -t -E D:\Dropbox\Projects\RealTimeEvents\src\rte\install\Install.log root@159.65.19.237 "cd /usr/local/share;sudo chmod 777 InstallServer_s1.sh;bash -x /usr/local/share/InstallServer_s1.sh InstallServer_s1.ini"
-@REM ssh -t -E Install.log rtinstall@159.65.19.237 "sudo -u rtinstall bash -x //usr/local/share/InstallServer_s2.sh InstallServer_s2.ini"
-@REM CALL UpdateWebServer_01_dev02.bat
---
+++ src/rte-installbackendserver/rte-installbackendserver_s1.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#clear
+
+# Exit on any error
+#set -e
+
+# Variables
+_ini=$1 # Ini configuration file specific to platform and machine
+_github_token=$2
+_github_org="RealTimeEvents"
+_repo_name="rte-installbackendserver"
+_github_url="https://$_github_token:@github.com/$_github_org/$_repo_name.git"
+_usr_dir=/usr
+_venv_base_dir="$_usr_dir/venv"
+_venv_name_dir="$_venv_base_dir/$_repo_name"
+_src_dir="/usr/local/src"
+#_rte_install_dir=$_rte_install_venv/lib/python3.8/site-packages/rte/install
+
+# Create installation sudo user
+_user_name=rtinstall
+#useradd $_user_name
+adduser $_user_name
+usermod -aG sudo $_user_name
+
+# Install pre-req packages
+apt update
+apt-get -y install python3-pip
+apt-get -y install mysql-server
+apt-get -y install python3-venv
+
+# Create virtual environment
+mkdir $_venv_base_dir
+chmod 777 $_venv_base_dir
+python3 -m venv --clear $_venv_name_dir
+source $_venv_name_dir/bin/activate
+pip install --upgrade InstallIt
+
+# Clone the private repository
+echo "Cloning the private repository from GitHub..."
+cd $_src_dir
+git clone $_github_url
+
+
+# Install and configure the server packages
+cd $_repo_name
+pip install -e .
+
+
+# Prompt to close the session
+echo Session completed
+echo Press any key to f_inish and quit
+read quit
+
+
+
+
+
+# Step 2: Navigate into the repository directory and install the python package
+#cd $REPO_NAME
+#echo "Installing the python package..."
+#pip install .
+
+# Step 3: Cleanup (Optional: Remove the cloned repository after installation)
+#cd ..
+#rm -rf $REPO_NAME
+#URS Prod / 2 GB Memory / 1 Intel vCPU / 50 GB Disk / LON1 - Ubuntu urs-prod-backend-clean
+#URS Prod / 2 GB Memory / 1 Intel vCPU / 50 GB Disk / LON1 - Ubuntu urs-prod-backend-clean
